92f17d2d0b6196248fb5d0deb39560ec913dd070
mdriaz60000 <mohammadriaz60000@gmail.com>
2023-10-04 09:29:14 +0600

    first commit

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from './../pages/Register/Register';
 
 const router = createBrowserRouter([
     {
@@ -9,8 +11,16 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element: <Home></Home>
-            }
+                element: <Home></Home>,
+            },
+            {
+                path: '/login',
+                element: <Login></Login>,
+            },
+            {
+                path: '/register',
+                element: <Register></Register>,
+            },
         ]
     }    
 ]);
